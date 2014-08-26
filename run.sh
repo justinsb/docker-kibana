bafe706bbe2b7d8bc2bb42944933bb074fafc644
Justin SB <justin@fathomdb.com>
2014-08-26 06:41:28 -0700

    Update to use docker links

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
-ES_PORT=${ES_PORT:-9200}
-ES_SCHEME=${ES_SCHEME:-\"http\"}
+
+ES_PORT=${ES_PORT_9200_TCP_PORT:-9200}
 
 cat << EOF > /usr/share/nginx/html/config.js
 define(['settings'],
 function (Settings) {
   return new Settings({
-    elasticsearch: "$ES_SCHEME://$ES_HOST:$ES_PORT",
-    default_route     : '/dashboard/file/logstash.json',
+    elasticsearch: "http://" + window.location.hostname + ":$ES_PORT",
+    default_route: '/dashboard/file/logstash.json',
     kibana_index: "kibana-int",
     panel_names: [
       'histogram',
